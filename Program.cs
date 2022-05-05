fdb9b0c946f43a0a938d3e0a7d5de853e0669ab8
DrEdgarvillegas <edgarvillegas5@gmail.com>
2022-05-05 19:02:17 -0300

    Ejer15Challenge1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double costoTotal, costoComprador, ganancia, impuestos = 0; 
+
+            Console.WriteLine("Ingrese el valor total del vehículo: ");
+
+            costoTotal = double.Parse(Console.ReadLine());
+
+            ganancia = costoTotal * 0.12;
+
+            impuestos = costoTotal * 0.06;
+
+            costoComprador = costoTotal + ganancia + impuestos;
+
+            Console.WriteLine("El costo  del consumidor es: " + costoComprador);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
